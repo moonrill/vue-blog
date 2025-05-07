465689db657f1f45318e3432a275ced72f29fc56
moonrill <arilramadani245@gmail.com>
2025-05-07 11:38:43 +0700

    feat: refactor the post card in blog

---
+++ src/components/PostCard.vue
@@ -0,0 +1,71 @@
+<script setup>
+import { ArrowUpRightIcon } from '@heroicons/vue/24/outline'
+
+defineProps({
+  authorAvatar: String,
+  authorName: String,
+  title: String,
+  description: String,
+  postDate: String,
+  postCategory: String,
+  postType: String,
+  link: String,
+  likeCount: String,
+  commentCount: String,
+  shareCount: String,
+  isLiked: { type: Boolean, default: false },
+})
+</script>
+<template>
+  <div
+    class="3xl:gap-[50px] border-dark-15 wrapper flex flex-col gap-[30px] border-b py-10 lg:flex-row 2xl:gap-[40px]"
+  >
+    <div class="flex h-fit items-center justify-between lg:w-[305px] 2xl:w-[390px]">
+      <div class="flex items-center gap-2 lg:gap-2.5 xl:gap-4">
+        <img
+          :src="authorAvatar"
+          alt="Avatar"
+          class="3xl:h-20 3xl:w-20 h-[60px] w-[60px] rounded-full"
+        />
+        <div>
+          <h4 class="text-lg leading-[150%] font-semibold tracking-[-0.03em]">{{ authorName }}</h4>
+          <p class="text-grey-60 text-sm leading-[150%] tracking-[-0.03em] 2xl:text-base">
+            {{ postCategory }}
+          </p>
+        </div>
+      </div>
+      <router-link
+        :to="link"
+        class="border-dark-15 bg-dark-08 flex h-fit items-center justify-center gap-1 rounded-lg border px-5 py-3.5 lg:hidden"
+      >
+        <div class="text-grey-60 text-sm">View {{ postType }}</div>
+        <ArrowUpRightIcon class="dark:text-yellow-55 3xl:size-6 size-5" />
+      </router-link>
+    </div>
+    <div class="flex w-full items-center justify-between xl:gap-[40px] 2xl:gap-[50px]">
+      <div class="3xl:gap-[30px] flex flex-col gap-5 2xl:gap-6">
+        <h4 class="text-grey-60 font-semibold 2xl:text-lg">{{ postDate }}</h4>
+        <div class="3xl:gap-2.5 flex flex-col gap-1 2xl:gap-1.5">
+          <h3 class="3xl:text-[26px] text-lg font-semibold 2xl:text-[22px]">
+            {{ title }}
+          </h3>
+          <p class="text-grey-60 3xl:text-lg text-sm 2xl:text-base">
+            {{ description }}
+          </p>
+        </div>
+        <div class="flex gap-2 2xl:gap-2.5">
+          <LikeButton :count="likeCount" :liked="isLiked" />
+          <CommentButton :count="commentCount" />
+          <ShareButton :count="shareCount" />
+        </div>
+      </div>
+      <router-link
+        :to="link"
+        class="border-dark-15 bg-dark-08 hover:bg-dark-15 hover:border-dark-25 hidden h-fit items-center justify-center gap-1 rounded-lg border px-5 py-3.5 lg:flex 2xl:gap-2.5 2xl:px-6 2xl:py-[18px]"
+      >
+        <div class="text-grey-60 2xl:text-base">View {{ postType }}</div>
+        <ArrowUpRightIcon class="dark:text-yellow-55 3xl:size-6 size-5" />
+      </router-link>
+    </div>
+  </div>
+</template>
--- src/components/home/BlogSection.vue
+++ src/components/home/BlogSection.vue
@@ -1,10 +1,8 @@
 <script setup>
-import LikeButton from '../LikeButton.vue'
-import CommentButton from '../CommentButton.vue'
-import ShareButton from '../ShareButton.vue'
-import ButtonSlider from '../ButtonSlider.vue'
-import { ArrowUpRightIcon } from '@heroicons/vue/24/outline'
 import HeadBanner from '@/components/ui/HeadBanner.vue'
+
+import ButtonSlider from '../ButtonSlider.vue'
+import PostCard from '../PostCard.vue'
 </script>
 <template>
   <HeadBanner
@@ -14,56 +12,44 @@ import HeadBanner from '@/components/ui/HeadBanner.vue'
     bannerTitle="Explore FutureTech's In-Depth Blog Posts"
   />
   <ButtonSlider />
-  <div class="wrapper">
-    <div class="3xl:gap-[50px] flex flex-col gap-[30px] py-10 lg:flex-row 2xl:gap-[40px]">
-      <div class="flex h-fit items-center justify-between lg:w-[305px] 2xl:w-[390px]">
-        <div class="flex items-center gap-2 lg:gap-2.5 xl:gap-4">
-          <img
-            src="../../assets/Imageavatar-2.jpg"
-            alt="Avatar"
-            class="3xl:h-20 3xl:w-20 h-[60px] w-[60px] rounded-full"
-          />
-          <div>
-            <h4 class="text-lg leading-[150%] font-semibold tracking-[-0.03em]">John Techson</h4>
-            <p class="text-grey-60 text-sm leading-[150%] tracking-[-0.03em] 2xl:text-base">
-              Quantum Computing
-            </p>
-          </div>
-        </div>
-        <router-link
-          to="/"
-          class="border-dark-15 bg-dark-08 flex h-fit items-center justify-center gap-1 rounded-lg border px-5 py-3.5 lg:hidden"
-        >
-          <div class="text-grey-60 text-sm">View Blog</div>
-          <ArrowUpRightIcon class="dark:text-yellow-55 3xl:size-6 size-5" />
-        </router-link>
-      </div>
-      <div class="flex w-full items-center justify-between xl:gap-[40px] 2xl:gap-[50px]">
-        <div class="3xl:gap-[30px] flex flex-col gap-5 2xl:gap-6">
-          <h4 class="text-grey-60 font-semibold 2xl:text-lg">October 15,2023</h4>
-          <div class="3xl:gap-2.5 flex flex-col gap-1 2xl:gap-1.5">
-            <h3 class="3xl:text-[26px] text-lg font-semibold 2xl:text-[22px]">
-              The Quantum Leap in Computing
-            </h3>
-            <p class="text-grey-60 3xl:text-lg text-sm 2xl:text-base">
-              Explore the revolution in quantum computing, its applications, and its potential
-              impact on various industries.
-            </p>
-          </div>
-          <div class="flex gap-2 2xl:gap-2.5">
-            <LikeButton count="1.2k" :liked="false" />
-            <CommentButton count="1.2k" />
-            <ShareButton count="1.2k" />
-          </div>
-        </div>
-        <router-link
-          to="/"
-          class="border-dark-15 bg-dark-08 hover:bg-dark-15 hover:border-dark-25 hidden h-fit items-center justify-center gap-1 rounded-lg border px-5 py-3.5 lg:flex"
-        >
-          <div class="text-grey-60 text-sm">View Blog</div>
-          <ArrowUpRightIcon class="dark:text-yellow-55 3xl:size-6 size-5" />
-        </router-link>
-      </div>
-    </div>
-  </div>
+  <PostCard
+    authorAvatar="https://i.pravatar.cc/600"
+    authorName="John Techson"
+    postCategory="Quantum Computing"
+    postDate="October 15,2023"
+    postType="Blog"
+    title="The Quantum Leap in Computing"
+    description="Explore the revolution in quantum computing, its applications, and its potential impact on various industries."
+    likeCount="1.2k"
+    commentCount="50"
+    shareCount="18"
+    link="/"
+    :isLiked="true"
+  />
+  <PostCard
+    authorAvatar="https://i.pravatar.cc/600"
+    authorName="Sarah Ethicist"
+    postCategory="AI Ethics"
+    postDate="November 5, 2023"
+    postType="Blog"
+    title="The Ethical Dilemmas of AI"
+    description="A deep dive into ethical challenges posed by AI, including bias, privacy, and transparency."
+    likeCount="32k"
+    commentCount="72"
+    shareCount="18"
+    link="/"
+  />
+  <PostCard
+    authorAvatar="https://i.pravatar.cc/600"
+    authorName="Astronomer X"
+    postCategory="Space Exploration"
+    postDate="December 10, 2023"
+    postType="Blog"
+    title="The Mars Colonization Challenge"
+    description="Exploring the technical and logistical challenges of human colonization on Mars."
+    likeCount="20k"
+    commentCount="31"
+    shareCount="12"
+    link="/"
+  />
 </template>
